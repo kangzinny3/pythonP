23a71ae5d04a3100dce6cc4913f568e49e6f49cd
kangzinny3 <kangzinny3@naver.com>
2019-09-10 12:20:11 +0900

    json folder

---
+++ json/json.py
@@ -0,0 +1,8 @@
+customer = {
+    'id' : 152352,
+    'name' : '강지은',
+    'history' : [
+        {'date' : '2019-09-10', 'item' : 'iPhone'},
+        {'date' : '2019-09-11', 'item' : 'Monitor'}
+    ]
+}
