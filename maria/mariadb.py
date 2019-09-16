527bd4751b1e0ba50d485040c4ee4298ff854853
kangzinny3 <kangzinny3@naver.com>
2019-09-16 10:18:30 +0900

    mariadb.py

--- maria/mariadb.py
+++ maria/mariadb.py
@@ -3,15 +3,17 @@
 import pymysql    # 설치 : 콘솔에서 pip install pymysql
 
 conn = pymysql.connect(host = 'localhost',
-                user = 'root',
-                password = 'qwer1234',
-                db = 'test')
+                        user = 'root',
+                        password = 'qwer1234',
+                        db = 'test')
 c = conn.cursor()
 sql = '''
     CREATE TABLE if not exists stocks
     (date text, trans text, symbol text, qty real, price real)
     '''
 
+c.execute(sql)
+sql = "insert into stocks values ('2006-03-28','buy','rhat',100,35.14)"
 c.execute(sql)
 conn.commit()
 conn.close()
