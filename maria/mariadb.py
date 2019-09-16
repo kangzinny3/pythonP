050b28df934c08d715d20b59c6105c47d0e9e89a
kangzinny3 <kangzinny3@naver.com>
2019-09-16 09:57:06 +0900

    mariaDB folder

---
+++ maria/mariadb.py
@@ -0,0 +1,17 @@
+# mariaDB 설치 : https://downloads.mariadb.org/mariadb/10.4.8/
+
+import pymysql    # 설치 : 콘솔에서 pip install pymysql
+
+conn = pymysql.connect(host = 'localhost',
+                user = 'root',
+                password = 'qwer1234',
+                db = 'test')
+c = conn.cursor()
+sql = '''
+    CREATE TABLE if not exists stocks
+    (date text, trans text, symbol text, qty real, price real)
+    '''
+
+c.execute(sql)
+conn.commit()
+conn.close()
