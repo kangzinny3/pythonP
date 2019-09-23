e88d4873bf8229df1aea992d80f2e8fffb9ba6be
kangzinny3 <kangzinny3@naver.com>
2019-09-23 09:47:05 +0900

    app1.py

--- flask/app1.py
+++ flask/app1.py
@@ -1,32 +1,23 @@
 from flask import Flask, request, render_template
+app = Flask(__name__)    # flask 객체 생성
 
-app = Flask(__name__)
+@app.route('/index')          # 클라이언트에서 서버로 요청할 때 요청하는 주소값 (http://www.~.com/ 일 때 / 뒷부분이 요청하는 주소값)
+def index():                  # 주소값(http://127.0.0.1:5000/)뒤에 index 입력하면 index 함수가 실행됨
+    return 'Hello world!'
 
-# @app.route('/user/<username>')
-# def show_username(username):
-#     return 'Username : %s' % username
+@app.route('/')  
+def home():
+    return render_template('home.html')
 
-# @app.route('/user/<username>/<int:age>') 
-# def show_user_profile(username, age): 
-#     return 'Username : %s, 나이 : %d' % (username, age) 
+@app.route('/formTest')
+def formTest():
+    return render_template('formTest.html')
 
-@app.route('/forminput')
-def form_input():
-    return render_template('form_input.html')
+if __name__ == '__main__':          # 파일 내에서 호출하면 __main__
+    app.run(debug=True, port=80)    # import해서 호출하면 파일명(__name__)
+  # debug=True -> html문서 등의 내용들이 바뀔 때 알아서 인지
+  # port=80 -> port번호를 변경하고자 하는 경우 입력
+# 그냥 app.run()만 해도 되지만, 그럴 경우에 다른 데서 import했을 때 자동 실행되버림
 
-@app.route('/method/', methods=['GET', 'POST'])   # methods 안적으면 get이 기본
-def login():                                      # /method 만 하고 /method/로 호출하면 Not Found
-    if request.method == 'POST':                  # /method/ 하고 /method로 호출해도 자동으로 /method/로 호출됨
-        name =  request.form['username'] 
-        pwd = request.form['password']
-        # return "Post %s, %s" % (name, pwd)
-        
-    else:
-        name = request.args.get('username')     # form_input.html에서 input에서 name으로 지정해줘야함
-        pwd = request.args.get('password')
-        # return "Get %s, %s" % (name, pwd)
-    return render_template('form_result.html', username=name, password=pwd)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=80)
-
+# app.py와 같은 위치에 templates폴더, teplates폴더 안에 html문서
+# app.py와 같은 위치에 static폴더, static폴더 안에 html제외 다른 문서들 -> 문서가 많아지면 하위폴더로 구분
