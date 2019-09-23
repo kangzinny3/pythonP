bf4c827c7fe4f6fa9e574dc5fe18b16dc5cfe732
kangzinny3 <kangzinny3@naver.com>
2019-09-23 11:44:37 +0900

    app.py

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mariadbfunc as db
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -10,7 +11,8 @@ def index():
 def formtest():
     return render_template('formtestblock.html')
 
-@app.route('/insertbook',methods=['POST'])
+# db와 연동
+@app.route('/insertbook', methods=['POST'])
 def insertbook():
     data = (request.form['title'], request.form['date'], request.form['publisher'], request.form['pages'], request.form['recommend'])
     db.create_table()
@@ -23,6 +25,12 @@ def booklist():
     books = db.all_books()
     print(books)
     return render_template('booklist.html', books = books)
+                                            # books를 booklist.html에 books라는 이름으로 넘겨줌
+
+@app.route('/content/<title>')
+def content(title):         # 꼭 매개변수(여기서는 title) 값 전달받아야 함
+    book = db.where_book(title)
+    return render_template('content.html', book = book)
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
