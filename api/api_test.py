2ac94c0bfd2b89171d74dd0e08f1a540d17d05bf
kangzinny3 <kangzinny3@naver.com>
2019-09-27 12:32:43 +0900

    api_test.py

--- api/api_test.py
+++ api/api_test.py
@@ -1,19 +1,41 @@
 import os
 import sys
 import urllib.request
+import json
+import sqlite3
 
-client_id = "YOUR_CLIENT_ID"
-client_secret = "YOUR_CLIENT_SECRET"
-encText = urllib.parse.quote("검색할 단어")
-url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
-# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
-request = urllib.request.Request(url)
-request.add_header("KkdqWUhHuEi2BcmU8tIB",client_id)
-request.add_header("nocwfffGkT",client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
-if(rescode==200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
+client_id = "iCfbmSA0wfvEiITT_TrQ"
+client_secret = "gHEKw5vUbM"
+encText = urllib.parse.quote("제주")
+for i in range(1, 1000, 100):
+    url = "https://openapi.naver.com/v1/search/news.json?display=100&start="+ str(i) +"&query=" + encText # json 결과
+                                    # display로 데이터 건수 조절
+    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if (rescode == 200):
+        response_body = response.read()
+        # print(response_body.decode('utf-8'))
+        data = response_body.decode('utf-8')
+        c = json.loads(data)   # 문자를 읽어서 파이썬 객체로 변경
+        items = c['items']
+        print(items[0])
+        # print(type(items))
+        # print(len(items))
+    else:
+        print("Error Code:" + rescode)
+
+conn = sqlite3.connect('D:/ai/pythonP/api/news.db') 
+c = conn.cursor()
+
+c.execute('''CREATE TABLE if not exists info
+            (title text, link text, description text)''')
+data = [[item['title'],item['link'],item['description']] for item in items]
+print(data)
+c.executemany('insert into info values(?, ?, ?)',data )
+conn.commit()  
+conn.close()
